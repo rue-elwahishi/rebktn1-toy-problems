0d51fd394f2951eb7a69e10ce1bbbc6d63eb98c5
mfmehdi <mfmehdi2@gmail.com>
2019-11-25 09:13:40 +0100

    done

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -5,7 +5,21 @@
  *   firstNonRepeatedCharacter('ABA'); // => 'B'
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
-
+//solution 1
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var obj={}
+  for (var i = 0; i < string.length; i++) {
+  	if(obj[string[i]]=== undefined ){
+  		obj[string[i]]=1
+  	}else{
+  	obj[string[i]]=obj[string[i]]+1;
+  }
+}
+
+for (key in obj) {
+	if(obj[key]===1)
+		return key
+}
+     return null                                                                       
 };
