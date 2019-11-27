413acedee581b0389ee9603bcba69bb0026f43fb
mfmehdi <mfmehdi2@gmail.com>
2019-11-27 09:35:27 +0100

    done

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -4,7 +4,7 @@
  * order that they appeared in `a`. Remember to skip spaces and characters you
  * have already encountered!
  *
- * Example: commonCharacters('acexivou', 'aegihobu')
+ * Example: commonCharacters('xacexivou', 'xaegihobu')
  * Returns: 'aeiou'
  *
  * Extra credit: Extend your function to handle more than two input strings.
@@ -13,5 +13,25 @@
 
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+ var str={}
+var result=""
+ var max=""
+ var min=""
+ if (string1.length > string2.length) {
+ 	max=string1;
+ 	min=string2;
+ }else{
+ 	max=string2;
+ 	min=string1;
+ }
+
+ for (var i = 0; i < max.length; i++) {
+ 	
+ 	if(min.indexOf(max[i])!==-1 && max[i]!== " "){
+ 		str[max[i]] = 1;
+ 	
+ 	}
+ }
+
+ return Object.keys(str).join("");
 };
