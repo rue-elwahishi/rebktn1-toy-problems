a21b30108412f63d7392f6fa8ac41f0e21ec8952
mfmehdi <mfmehdi2@gmail.com>
2019-12-03 09:50:56 +0100

    --

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,6 +10,32 @@
  * console.log(index); // null
  */
 
-var binarySearch = function (array, target) {
-};
 
+var binarySearch = function (array, target) {
+	 var middelIndex = arguments[2]||Math.floor(array.length/2)
+//console.log(middelIndex)
+if((middelIndex!==array.length-1) ||  (middelIndex!== 0))
+{
+	if(target === array[middelIndex])  return middelIndex
+
+	if(target > array[middelIndex]){
+		middelIndex =Math.floor(((array.length-middelIndex)/2)+middelIndex);
+		if(middelIndex === array.length-1) {
+			if(array[middelIndex] === target) return true
+				return null
+		}
+		binarySearch(array,target,middelIndex)
+		
+	}else{ 
+		if(target < array[middelIndex]){
+		middelIndex =Math.floor(middelIndex/2);
+			if(middelIndex === 0) {
+			if(array[middelIndex] === target) return true
+				return null
+		}
+		binarySearch(array,target,middelIndex)}
+	
+}}
+	
+	
+};
