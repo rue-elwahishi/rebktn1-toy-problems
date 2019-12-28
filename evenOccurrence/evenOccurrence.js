7ce9ff0958f8151628fa88dea3adb01ce4847848
ghostrib <sh4rkbot@gmail.com>
2019-12-28 08:52:29 +0100

    evenOccurrence

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -5,4 +5,5 @@ Find the first item that occurs an even number of times in an array. Remember to
 evenOccurrence([ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]) // 2
 evenOccurrence([ "cat", "dog", "dig", "cat" ]) // "cat"
 evenOccurrence([[1,2,3], [1,2], [1,2,3]]) // [1,2,3]
+evenOccurrence([{a: 1, b: 2}, {a:1, b:3}, {a:1, b:2}]) // {a:1, b:2}
 */
