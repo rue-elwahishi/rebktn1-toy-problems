2144c20cdcd99cea9eed36cc049d8589e73ab049
ghostrib <sh4rkbot@gmail.com>
2019-12-31 08:39:36 +0100

    zero sum

---
+++ zeroSum/zeroSum.js
@@ -0,0 +1,9 @@
+/*
+Zero Sum
+Given an array of negative/positive integers, return true if there exists two numbers whose sum is zero.
+
+Constraints: Solve this linear time complexity O(n)
+
+zeroSum([ 1, 3, 2, -3 ]) // true
+zeroSum([ 5, 7, 2, 9 ]) // false
+*/
