476429b27ab45a5d9f5980b9fd343682b150c13f
ghostrib <sh4rkbot@gmail.com>
2020-01-03 08:35:20 +0100

    longestRun

---
+++ longestRun/longestRun.js
@@ -0,0 +1,15 @@
+/*
+Longest Run
+
+Write a function that, given a string, finds the longest run of identical characters and returns an array containing the start and end indices of that run. 
+If there are two runs of equal length, return the first one. Return [0,0] for no runs.
+
+longestRun("abbbcc") // [ 1, 3 ]
+longestRun("aabbc") // [ 0, 1 ]
+longestRun("") // [ 0, 0 ]
+longestRun("mississippi") // [ 2, 3 ]
+longestRun("abcdefgh") // [ 0, 0 ]
+longestRun("abccccccc") // [ 2, 8 ]
+*/
+
+function longestRun(string) {}
