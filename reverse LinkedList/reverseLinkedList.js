2ca889a2aae2567f5f4475930012e06fde284478
yousefalshabah <yousefalshabah@hotmail.com>
2020-01-23 08:51:08 +0100

    /*

---
+++ reverse LinkedList/reverseLinkedList.js
@@ -0,0 +1,30 @@
+/*
+Write a function that reverses a linked list.
+​
+Explanation:
+​
+Given the below linked list:
+​
+	A -> B -> C -> D -> E -> null
+​
+Return:
+​
+	E -> D -> C -> B -> A -> null
+​
+Constraint 1: Do this in linear time
+Constraint 2: Do this in constant space
+Constraint 3: Do not mutate the original nodes by adding any new properties
+*/
+​
+// Helpers
+​
+function Node (val) {
+  var obj = {};
+  obj.value = val || null;
+  obj.next = null;
+  return obj;
+}
+​
+var reverseLinkedList = function(linkedList) {
+  // your code here...
+};
