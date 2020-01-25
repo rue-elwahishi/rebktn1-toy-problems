b41c09026df735dcd391a472f745da5223f439bc
ghostrib <sh4rkbot@gmail.com>
2020-01-25 08:47:52 +0100

    telephoneWords

---
+++ telephoneWords/telephoneWords.js
@@ -0,0 +1,15 @@
+/*
+Each number key on a standard phone keypad has a set of Latin letters written on it as well: http://en.wikipedia.org/wiki/File:Telephone-keypad2.svg
+
+Businesses often try to come up with clever ways to spell out their phone number in advertisements to make it more memorable. But there are a lot of combinations!
+
+Write a function that takes up to four digits of a phone number, and returns a list of all of the words (in alphabetical order) that can be written on the phone with that number (you should return all permutations, not only English words).
+
+
+telephoneWords("0002") // [ "000A", "000B", "000C" ]
+telephoneWords("1123") // [ "11AD", "11AE", "11AF", "11BD", "11BE", "11BF", "11CD", "11CE", "11CF" ]
+*/
+
+function telephoneWords(digitString) {
+  // your code here...
+}
