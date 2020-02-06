7d5aa12206ca289e9b428f0735fb381fa0587d4b
ghostrib <sh4rkbot@gmail.com>
2020-02-06 08:40:50 +0100

    militaryTime

---
+++ militaryTime/militaryTime.js
@@ -0,0 +1,15 @@
+/*
+Military Time
+Given a string that represents time in hours and minutes, convert the string to military time (if necessary).
+
+Examples: 
+
+toMilitary("3:00pm") // "15:00"
+toMilitary("9:15am") // "09:15"
+toMilitary("12:00am") // "00:00"
+toMilitary("04:00") // "04:00"
+*/
+
+function toMilitary(time) {
+  // your code here...
+}
