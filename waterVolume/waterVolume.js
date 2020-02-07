1ca79351417acbb9e108d414b2efae1c96769726
ghostrib <sh4rkbot@gmail.com>
2020-02-07 08:42:35 +0100

    waterVolume

---
+++ waterVolume/waterVolume.js
@@ -0,0 +1,23 @@
+/*
+You are given an array of numbers that each represent the height of an adjoining building. During torrential rain, water collects in the spaces between buildings. What is the maximum amount of water collected by the buildings represented by the array?
+
+Explanation: An input of [3,0,3] can be visualized as such:
+
+ __    __
+|  |//|  |
+|  |//|  |
+|  |//|  |
+‾‾‾‾‾‾‾‾‾‾
+The area of water in the above picture (and the result of calling volume([3,0,3])) is 3.
+
+Challenge: do this in O(n) with < 2 pass-throughs
+
+volume([ 3, 0, 3 ]) // 3
+volume([ 1, 2, 3, 4, 0, 5, 3, 1 ]) // 4
+volume([1,2,3,2,1]) // 0
+
+*/
+
+function volume(heights) {
+  // your code here...
+}
